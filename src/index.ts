0084b997893939b965c74d3be426981625f1c569
burry439 <burry439@gmail.com>
2020-06-23 13:04:00 +0300

    'quickdeploy'

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import http, { Server } from 'http'
 import cors from 'cors'
 import controllers from "./controllers/baseController";
-//import SocketInstance from "./socketIo/socketInstance";
+import SocketInstance from "./socketIo/socketInstance";
 
 dotenv.config()
 
@@ -13,7 +13,7 @@ class ExpressServer {
     private app: express.Application;
     private router : express.Router;
     private server : Server;
-    //public socketInstance : SocketInstance;
+    public socketInstance : SocketInstance;
     constructor(){
 
       this.app  = express () 
@@ -31,18 +31,20 @@ class ExpressServer {
         res.send(err.toString())
       })
 
-      this.app.get('/*', function(req, res) {
-          //come up with better soulutions
-          res.sendFile(path.join("build/frontend/index.html"),{ root: process.env.ROOT_FOLDER }, function(err) {
-            if (err) {
-              res.status(500).send(err)
-            }
-          })
-        })
+ 
   
       this.server   = http.createServer ( this.app )
       this.server.listen ( process.env.PORT || 8080 )
-      //this.socketInstance = SocketInstance.getSocketInstance(this.server)
+      this.socketInstance = SocketInstance.getSocketInstance(this.server)
+
+      this.app.get('/*', function(req, res) {
+        //come up with better soulutions
+        res.sendFile(path.join("build/frontend/index.html"),{ root: process.env.ROOT_FOLDER }, function(err) {
+          if (err) {
+            res.status(500).send(err)
+          }
+        })
+      })
       console.log ( '=====================================' )
       console.log ( 'SERVER SETTINGS:' )
       console.log ( `Server running at - localhost:${ process.env.PORT }` )
